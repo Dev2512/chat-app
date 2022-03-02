5c89391c77ea220ca144f6b512e7b9eeb9425c31
Dev2512 <84842498+Dev2512@users.noreply.github.com>
2022-03-02 19:13:45 +1100

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,8 @@
+var username = "";
+function addUser()
+{
+     username = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "Kwitter_room.html";
+}
